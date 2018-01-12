a90e63c5932dc57d5f806bdbe630ed5eee01d75d
Papatpon <papatpon@trinityroots.co.th>
2018-01-12 17:11:25 +0700

    add php index file

--- index.php
+++ index.php
@@ -11,4 +11,13 @@ foreach($html->find('img') as $element)
 // Find all links 
 foreach($html->find('a') as $element) 
        echo $element->href . '<br>';
- ?>
+
+// Create DOM from string
+$html = str_get_html('<div id="hello">Hello</div><div id="world">World</div>');
+
+$html->find('div', 1)->class = 'bar';
+
+$html->find('div[id=hello]', 0)->innertext = 'foo';
+
+echo $html; // Output: <div id="hello">foo</div><div id="world" class="bar">World</div>
+?>
