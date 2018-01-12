ce74fad6edf292ff91ff9eb02a654b0f17a39dc8
Papatpon <papatpon@trinityroots.co.th>
2018-01-12 17:09:47 +0700

    add php index file

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Create DOM from URL or file
 echo 'test test';
 
-$html = file_get_html('http://www.google.com/');
+$html = file_get_html('https://www.google.com/');
 
 // Find all images 
 foreach($html->find('img') as $element) 
