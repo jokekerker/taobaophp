e128c4defdd56d08f17bbb871c1cc7af095f3dcd
Papatpon <papatpon@trinityroots.co.th>
2018-01-15 19:26:03 +0700

    fix

--- index.php
+++ index.php
@@ -22,23 +22,26 @@ use Sunra\PhpSimple\HtmlDomParser;
 
 // Create DOM from URL or file
 // echo 'test test';
+?>
 
-//$html = HtmlDomParser::file_get_html('https://item.taobao.com/item.htm?spm=a21bo.7929913.198967.22.473f8c80RNCXEf&id=557488171107');
-$urlstr = 'https://item.taobao.com/item.htm?spm=a21wu.241046-global.4691948847.3.7a6cdb29U1yA27&scm=1007.15423.84311.100200300000005&id=561065846240&pvid=74d9c0b8-89a6-4c85-8b4a-235508b49$508b49da2';
-$html = HtmlDomParser::file_get_html($urlstr);
-//echo $html;
-// $html = HtmlDomParser::str_get_html('<div id="hello">Hello</div><div id="world">World</div>');
-//$html->encoding = 'utf-8';
+<form method='get' action='index.php'>
+    <input type="text" id="text_first" name="text_first" /> <br />
+    <input type="submit" id="submit" />
+</form>
 
-foreach ($html->find('div[class=tb-item-info tb-clear]') as $element){
-	$element->encoding = 'utf-8';
-    echo $element . '<br>';
+<?php
+
+if(isset($_GET['text_first'])) {
+    // $urlstr = 'https://item.taobao.com/item.htm?spm=a21wu.241046-global.4691948847.3.7a6cdb29U1yA27&scm=1007.15423.84311.100200300000005&id=561065846240&pvid=74d9c0b8-89a6-4c85-8b4a-235508b49$508b49da2';
+    $urlstr = $_GET['text_first'];
+	$html = HtmlDomParser::file_get_html($urlstr);
+
+
+	foreach ($html->find('div[class=tb-item-info tb-clear]') as $element){
+		$element->encoding = 'utf-8';
+	    echo $element . '<br>';
+	}
 }
-//require __DIR__ . 'vendor/autoload.php';
-
-//$log = new Monolog\Logger('name');
-//$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-//$log->addWarning('Foo');
 
 
 ?>
